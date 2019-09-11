5de7b6c60397542c534cb52a41f7d0e54f7bad7e
William Chen <chen.will7@gmail.com>
2019-09-11 12:27:29 -0700

    Logo View added

--- Logo/Logo/LogoView.swift
+++ Logo/Logo/LogoView.swift
@@ -14,7 +14,7 @@ class LogoView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
     }
     
 
